ba0843caf9d8b2858171b7dbfefcdf54b064fd0c
Muhta Nuryadi <nuryadi.muhta@gmail.com>
2023-04-19 21:56:40 +0700

    progress

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -9,29 +9,32 @@
   <body>
 
 <div class="container">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="/home">Navbar</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" href="#">Home</a>
+                <a class="nav-link active" href="/home">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link" href="/about">About</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Blog</a>
+                <a class="nav-link" href="/blog">Blog</a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-    <h1>Hello, world!</h1>
+    <div class="container mt-4">
+        @yield('container')
+    </div>
+   
 
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', function () {
-    return view('hello');
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function(){
+    return view('about');
+});
+
+Route::get('/blog', function(){
+    return view('blog');
 });
 
 
